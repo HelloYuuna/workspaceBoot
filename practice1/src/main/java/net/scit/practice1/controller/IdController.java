e3c2c35bda255df78a877c1ff04e1167aa7f53cd
HelloYuuna <hah1236@hotmail.co.jp>
2022-07-12 16:53:55 +0900

    20220712

--- practice1/src/main/java/net/scit/practice1/controller/IdController.java
+++ practice1/src/main/java/net/scit/practice1/controller/IdController.java
@@ -28,16 +28,34 @@ public class IdController {
     }
 
     @PostMapping("/prac/idResult")
-    public String idResult(String name, String num, Model model) {
-        System.out.println(name + ", " + num);
-
+    public String idResult(String usrname, String myId, Model model) {
+        log.info("유저아이디, 주민번호" + usrname + myId);
         /* 날짜 생성 */
 //        근래에는 지양하는 Calendar 와 Date 객체
 //        Calendar calendar = Calendar.getInstance();
 //        int year = calendar.get(Calendar.YEAR);
+        /* 현재 년도 */
         LocalDate ld = LocalDate.now();
         int year = ld.getYear();
         log.info("year" + year);
+
+        /* 주민번호 자리수 */
+        if (myId.length() != 14) {              // - 포함 14자리
+            return "redirect:/id/identifier";
+        }
+
+        /* - 포함 체크 */
+        if (myId.indexOf("-") != 6) {           // 6번째 자리에 -가 없다면
+            return "redirect:/id/identifier";
+        }
+
+        /* 성별 체크 */
+        char gender = myId.charAt(7);           // 0부터 카운트
+        if (gender < '1' || gender > '4') {
+            return "redirect:/id/identifier";
+        }
+
+        /* 생년월일 뽑기 */
 
         return "/id/idResult";
     }
